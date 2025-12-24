c6f0c99a916d2ac504b1c745ce842c6ec572de23
LAIN-EXE666 <qq3435795244@163.com>
2025-12-24 19:58:25 +0800

    fix

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -60,16 +60,20 @@
     window.addEventListener("DOMContentLoaded", () => {
         // Theme switch
         const lamp = document.getElementById("mode");
-
-        lamp.addEventListener("click", () => toggleTheme());
+        if (lamp) {
+            lamp.addEventListener("click", () => toggleTheme());
+        }
 
         // Blur the content when the menu is open
         const cbox = document.getElementById("menu-trigger");
-
-        cbox.addEventListener("change", function () {
-            const area = document.querySelector(".wrapper");
-            if (this.checked) return area.classList.add("blurry");
-            area.classList.remove("blurry");
-        });
+        if (cbox) {
+            cbox.addEventListener("change", function () {
+                const area = document.querySelector(".wrapper");
+                if (area) {
+                    if (this.checked) return area.classList.add("blurry");
+                    area.classList.remove("blurry");
+                }
+            });
+        }
     });
 })();
